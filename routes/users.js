8aaaa9321735bebc88d4291bbb5235d1c667ee60
Ayush Priyam <ayushpriyam24@gmail.com>
2023-12-15 14:45:07 +0530

    databse issue resolved

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,16 @@ const mongoose=require("mongoose");
 const plm=require("passport-local-mongoose")
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URL)
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
-  });
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((err) => {
+  console.error('Error connecting to MongoDB:', err);
+});
 
 const userSchema=mongoose.Schema({
 	
